1d1088f1939033030b628076aa66dfa514893f17
Hiromasa Kato <hka@trackman.com>
2019-09-12 15:05:52 +0200

    race search scraping fixed

--- itra-scrape.py
+++ itra-scrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from collections import namedtuple
 
 class ITRAScraper(object):
 
@@ -17,6 +18,7 @@ class ITRAScraper(object):
 
 
     def get_result(self, idedition):
+        runner = namedtuple('Runner', ['name', 'time', 'overall_rank', 'sex', 'performance_index', 'nationality'])
         data = {'mode':'getCourse', 'idedition':str(idedition)}
         r = self.session.post('https://itra.run/fiche.php', data=data)
         if not r:
@@ -25,7 +27,7 @@ class ITRAScraper(object):
         soup = BeautifulSoup(r.text, features='html.parser')
         result = []
         for row in soup.select('tr.odd, tr.even'):
-            result.append([data.text.strip() for data in row.find_all('td')])
+            result.append(runner(*[data.get_text().strip() for data in row.find_all('td')]))
         return result
 
 
@@ -39,24 +41,35 @@ class ITRAScraper(object):
         races = []
         ref = re.compile('^.*getResult\(\'([0-9]+)\'\);.*$')
         for race in soup.select('div.race'):
-            name = race.find('h2').text.strip()
+            name = race.find('h2').get_text().strip()
             idedition = ref.search(race.find('a').attrs['onclick']).group(1)
 
-            where, when_info = race.find_all('div')[:2]
-            where = where.text.strip()
-            when = when_info.text.strip()
-            races.append([idedition, name, where, when])
+            where, when = race.find_all('div')[:2]
+            where = where.get_text().strip()
+            info = when.span.extract().get_text().strip()
+            when = when.get_text().strip()
+            races.append({'idedition':idedition, 'name':name, 'where':where, 'when':when, 'info':info})
         return races
 
 
 def test(username, password):
+    import sys
+    import csv
+
     itra = ITRAScraper(username, password)
 
     races = itra.search_races('01/01/2019', '11/09/2019', 'UTMB')
-    print(races)
+    writer = csv.DictWriter(sys.stdout, fieldnames=races[0].keys())
+    for race in races:
+        writer.writerow(race)
 
-    #result = itra.get_result('39879')
-    #print(result)
+    return
+
+    result = itra.get_result('39879')
+    writer = csv.DictWriter(sys.stdout, fieldnames=result[0]._fields)
+    writer.writeheader()
+    for runner in result:
+        writer.writerow(runner._asdict())
 
 
 if __name__ == '__main__':
@@ -65,6 +78,5 @@ if __name__ == '__main__':
     parser.add_argument('--username')
     parser.add_argument('--password')
     args = parser.parse_args()
-    print(args.username, args.password)
     test(args.username, args.password)
 
